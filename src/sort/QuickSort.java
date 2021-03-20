46987cbe83aab3b7b478be9c809429f02c0d2e89
sekijohn10 <50596878+sekijohn10@users.noreply.github.com>
2021-03-19 19:24:32 -0600

    Add files via upload

---
+++ src/sort/QuickSort.java
@@ -0,0 +1,41 @@
+package sort;
+
+import java.util.Arrays;
+
+public class QuickSort<T extends Comparable<T>> {
+    private T[] output;
+    private int comparisons;
+
+    public SortResult<T> sort(T[] input) {
+        output = Arrays.copyOf(input, input.length);
+        branchDown(output, 0, output.length - 1);
+        return new SortResult<>(comparisons, input, output);
+    }
+
+    private void branchDown(T[] a, int lo, int hi) {
+        if (lo < hi) {
+            int p = partition(a, lo, hi);
+            branchDown(a, lo, p - 1);
+            branchDown(a, p + 1, hi);
+        }
+    }
+    private int partition(T[] a, int lo, int hi) {
+        T pivot = a[hi];
+        int i = lo;
+        for (int j = lo; j <= hi; j++) {
+            if (a[j].compareTo(pivot) < 0) {
+                swap(j, i);
+                i++;
+            }
+            comparisons++;
+        }
+        swap(i, hi);
+        return i;
+    }
+
+    private void swap(int in1, int in2) {
+        T temp = output[in1];
+        output[in1] = output[in2];
+        output[in2] = temp;
+    }
+}
