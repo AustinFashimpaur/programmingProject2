577fcf242ada47193f4d2a399ee1200cd0344c04
mcharris <mcharris@adobe.com>
2021-03-22 21:48:30 -0600

    Finish test driver

--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -1,61 +1,61 @@
 package sort;
 
-public class HeapSort<T extends Comparable<T>> implements Sort<T>
-{
-    private T[] output;
-    private int comparisons = 0;
-    
+//public class HeapSort<T extends Comparable<T>> implements Sort<T>
+//{
+//    private T[] output;
+//    private int comparisons = 0;
+//
 //    public SortResult<T> sort(T[] input)
 //    {
 //        output = Arrays.copyOf(input, input.length);
 //        return new SortResult<>(comparisons, input, output);
 //    }
     
-    public void sort(T[] input)
-    {
-        int n = input.length;
- 
-        for (int i = n/2 - 1; i >= 0; i--)
-        {
-            heapify(input, n, i);
-            comparisons++;
-        }
-
-        for (int i = n - 1; i > 0; i--) 
-        {
-            swap(0, i);
-            heapify(input, i, 0);
-            comparisons++;
-        }
-    }
-    
-    public void heapify(T[] input, int k, int l)
-    {
-        int largest = l;
-        int v = 2 * l + 1;
-        int r = 2 * l + 2;
- 
-        if (v < k && input[v].compareTo(input[largest]) < 0)
-        {
-            largest = v;
-        }
-
-        if (r < k && input[r].compareTo(input[largest]) > 0)
-        {
-            largest = r;
-        }
-
-        if (largest != l) 
-        {
-            swap(l, largest);
-        }
-    }
-    
-    private void swap(int nf, int ns) 
-    {
-        T temp = output[nf];
-        output[nf] = output[ns];
-        output[ns] = temp;
-    }
-    
-}
+//    public void sort(T[] input)
+//    {
+//        int n = input.length;
+//
+//        for (int i = n/2 - 1; i >= 0; i--)
+//        {
+//            heapify(input, n, i);
+//            comparisons++;
+//        }
+//
+//        for (int i = n - 1; i > 0; i--)
+//        {
+//            swap(0, i);
+//            heapify(input, i, 0);
+//            comparisons++;
+//        }
+//    }
+//
+//    public void heapify(T[] input, int k, int l)
+//    {
+//        int largest = l;
+//        int v = 2 * l + 1;
+//        int r = 2 * l + 2;
+//
+//        if (v < k && input[v].compareTo(input[largest]) < 0)
+//        {
+//            largest = v;
+//        }
+//
+//        if (r < k && input[r].compareTo(input[largest]) > 0)
+//        {
+//            largest = r;
+//        }
+//
+//        if (largest != l)
+//        {
+//            swap(l, largest);
+//        }
+//    }
+//
+//    private void swap(int nf, int ns)
+//    {
+//        T temp = output[nf];
+//        output[nf] = output[ns];
+//        output[ns] = temp;
+//    }
+//
+//}
--- src/sort/TestDriver.java
+++ src/sort/TestDriver.java
@@ -20,39 +20,46 @@ package sort;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class TestDriver {
-
     public static void main(String[] args) throws FileNotFoundException {
-        File nIs4 = new File("n4.txt");
-        File nIs6 = new File("n6.txt");
-        File nIs8 = new File("n8.txt");
-
-        HeapSort<Integer> heapSorter = new HeapSort<>();
+//        HeapSort<Integer> heapSorter = new HeapSort<>();
         MergeSort2<Integer> mergeSorter = new MergeSort2<>();
         QuickSort<Integer> quickSorter = new QuickSort<>();
         ShakerSort<Integer> shakerSorter = new ShakerSort<>();
+        List<Sort<Integer>> sorters = new ArrayList<>();
+//        sorters.add(heapSorter);
+        sorters.add(mergeSorter);
+        sorters.add(quickSorter);
+        sorters.add(shakerSorter);
 
-        // permutation generator
+        File nIs4 = new File("./files/n4.csv");
+        File nIs6 = new File("./files/n6.csv");
+        File nIs8 = new File("./files/n8.csv");
+        List<File> files = new ArrayList<>();
+        files.add(nIs4);
+        files.add(nIs6);
+        files.add(nIs8);
 
-        // create list of all permutations for n=4
-        // call each sort for every permutation of n=4
-        Scanner nIs4Reader = new Scanner(nIs4);
-        while (nIs4Reader.hasNextLine()) {
-            int[] nums = Arrays.stream(nIs4Reader.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
-            Integer[] nums2 = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-            heapSorter.sort(nums2);
-            mergeSorter.sort(nums2);
-            quickSorter.sort(nums2);
-            shakerSorter.sort(nums2);
+        for (Sort<Integer> sorter : sorters) {
+            System.out.println("------ " + sorter.getClass().getName() + " ------");
+            for (File file : files) {
+                System.out.println("------ " + file.getName() + " -------");
+                Scanner reader = new Scanner(file);
+                while (reader.hasNextLine()) {
+                    Integer[] nums = Arrays.stream(reader.nextLine().replaceAll("\\s", "").split(","))
+                            .mapToInt(Integer::parseInt)
+                            .boxed()
+                            .toArray(Integer[]::new);
+                    System.out.println(sorter.sort(nums));
+                }
+                System.out.println("\n\n");
+            }
+            System.out.println("\n\n\n");
         }
-
-        // create list of all permutations for n=6
-        // call each sort for every permutation of n=6
-
-        // create list of all permutations for n=8
-        // call each sort for every permutation of n=8
     }
 }
